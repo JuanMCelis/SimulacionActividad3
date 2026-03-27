04a4bb532cb6c33c49b23fb8aa2117867d1d26be
JuanMCelis <163361602+JuanMCelis@users.noreply.github.com>
2026-03-27 01:44:31 -0500

    Update ClimateEventsManager.cs

--- Assets/Scripts/ClimateEventsManager.cs
+++ Assets/Scripts/ClimateEventsManager.cs
@@ -15,7 +15,8 @@ public class ClimateEventsManager : MonoBehaviour
     public event Action<ClimateState> OnClimateChanged;
 
     //Tiempo que va a durar cada clima
-    public float durationPerClimate = 10f;
+    public float minTime = 10f;
+    public float maxTime = 20f;
     private float timer;
 
     // Efectos visuales de los climas para sensación de ejecución
@@ -23,27 +24,26 @@ public class ClimateEventsManager : MonoBehaviour
     public ParticleSystem stormParticles;
     public UnityEngine.UI.Image lightningImage;
 
-    //Se ejecuta al iniciar el objeto
-    void Awake()
-    {
-        // Linea para evitar duplicados)
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-    }
-
+ 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        timer = durationPerClimate;
+        SetClimate(ClimateState.Clear); //Esta linea lo que hace es que siempre el clima empezara definido como despejado
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        // Cuando el temporizador  para cambiar al clima llega a 0 el clima va a cambiar
+        if (timer <= 0f)
+        {
+            NextClimate(); //Llama a la funcion de cambio de clima
+
+            // Se define un tiempo aleatorio entre 15 segundos o 25 segundos
+            timer = UnityEngine.Random.Range(15f, 25f);
+        }
     }
 }
