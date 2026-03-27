614f76cc0f18034aa75851d54d8fa85b48c4390e
JuanMCelis <163361602+JuanMCelis@users.noreply.github.com>
2026-03-27 02:19:56 -0500

    Update ClimateEventsManager.cs

--- Assets/Scripts/ClimateEventsManager.cs
+++ Assets/Scripts/ClimateEventsManager.cs
@@ -8,7 +8,7 @@ public class ClimateEventsManager : MonoBehaviour
     public static ClimateEventsManager Instance { get; private set; }
 
     // Tipos de clima (despejado, lluvia y tormenta)
-    public enum ClimateState { Clear, Rain, Storm }
+    public enum ClimateState { Clear, Rain, Storm } //Enumera los climas del 0 al 2 siendo 0 - Despejado, 1 - Lluvia y 2 - tormenta
     public ClimateState CurrentClimate { get; private set; } = ClimateState.Clear;
 
     //Evento (opcional, por si otros scripts quieren reaccionar)
@@ -28,7 +28,7 @@ public class ClimateEventsManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        timer = durationPerClimate;
+        timer = UnityEngine.Random.Range(minTime, maxTime);
         SetClimate(ClimateState.Clear); //Esta linea lo que hace es que siempre el clima empezara definido como despejado
     }
 
@@ -36,14 +36,94 @@ public class ClimateEventsManager : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
-
+        
         // Cuando el temporizador  para cambiar al clima llega a 0 el clima va a cambiar
         if (timer <= 0f)
         {
             NextClimate(); //Llama a la funcion de cambio de clima
 
-            // Se define un tiempo aleatorio entre 15 segundos o 25 segundos
-            timer = UnityEngine.Random.Range(15f, 25f);
+            // Se define un tiempo aleatorio entre 10 segundos a 20 segundos
+            timer = UnityEngine.Random.Range(minTime, maxTime);
         }
     }
+
+
+    // Función donde se ejecutara la logica del cambio climatico
+    void NextClimate()
+    {
+
+        int randomClimate = UnityEngine.Random.Range(0, 3); // Nos bota un numero del 0 al 2
+        SetClimate((ClimateState)randomClimate); //Segun el numero que sale se escoge el clima al que va a pasar. Llama a la enum del inicio y si sale 0 despejado 1 lluvia y 2 tormenta.
+    }
+
+    void SetClimate(ClimateState newClimate)
+    {
+        CurrentClimate = newClimate; //Aqui se va a guardar el clima que se va a ejecutar. 
+
+        // Los efectos del clima anterior se borrarar de la pantalla
+        if (rainParticles != null) //Si hay lluvia la desactiva
+            rainParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+
+        if (stormParticles != null) //Si hay tormenta la desactiva
+            stormParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+
+        if (lightningImage != null) //El rayo lo hace invisible 
+            lightningImage.color = new Color(1, 1, 1, 0);
+
+        // Un switch para activar un nuevo clima
+        switch (newClimate)
+        {
+            case ClimateState.Clear: //Si se recibe de climateState el numero 0, el clima se despeja
+                Debug.Log("Clima despejado");
+                break;
+
+            case ClimateState.Rain: //Si se recibe de climateState el numero 1, el clima es lluvia
+                Debug.Log("Lluvia");
+                if (rainParticles != null)
+                    rainParticles.Play(); // Salen las particulas de lluvia
+                break;
+
+            case ClimateState.Storm:
+                Debug.Log("Tormenta");  //Si se recibe de climateState el numero 0, el clima es tormenta
+                if (stormParticles != null)
+                    stormParticles.Play(); //Hace que salgan las cosas para simular la tormenta
+
+                if (lightningImage != null)  
+                    StartCoroutine(LightningFlash());  //Activa flashes para simular rayos
+                break;
+        }
+
+        // Avisar a otros scripts
+        OnClimateChanged?.Invoke(newClimate);
+    }
+
+
+    // Funcion para el efecto de rayo como un parpadeo rapido en pantalla
+    System.Collections.IEnumerator LightningFlash() //Crea una funcion que se ejecuta con pausas mientras sigue funcionando la simulación.
+    {
+        while (CurrentClimate == ClimateState.Storm) //Un while que funciona si el clima es tormenta ----> Seria algo tipo; mientras el clima sea tormenta ejecuta lo de down
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(3f, 6f)); // Está linea lo que nos permite que haya un tiempo tipo como de recarga para que vuelva a salir el efecto, esta configuarada
+            //entre 3 a 6 segundos
+
+            if (lightningImage != null)
+            {
+                lightningImage.color = new Color(1, 1, 1, 0.8f);
+                yield return new WaitForSeconds(0.1f);
+                lightningImage.color = new Color(1, 1, 1, 0);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
